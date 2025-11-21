31f64e9253e6e4cf932614fe4661896ff73407c7
3x-haust <lyusungyun@icloud.com>
2025-11-21 14:22:34 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "RooT_Server"
---
+++ src/main/java/io/github/_3xhaust/root_server/RooTServerApplication.java
@@ -0,0 +1,14 @@
+package io.github._3xhaust.root_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+RooTServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RooTServerApplication.class, args);
+    }
+
+}
