7cd6bd532164389a8ce58057666faa445f9dba30
KwakJakyeong <ggyul847@gmail.com>
2025-12-28 17:35:56 +0900

    feat : Community Controller post >> ㅜnot yet

--- src/main/java/io/github/_3xhaust/root_server/domain/community/controller/CommunityController.java
+++ src/main/java/io/github/_3xhaust/root_server/domain/community/controller/CommunityController.java
@@ -1,7 +1,12 @@
 package io.github._3xhaust.root_server.domain.community.controller;
 
+import io.github._3xhaust.root_server.domain.community.dto.req.CreateCommunityRequest;
+import io.github._3xhaust.root_server.domain.community.dto.res.CommunityListResponse;
+import io.github._3xhaust.root_server.domain.community.dto.res.CommunityResponse;
 import io.github._3xhaust.root_server.domain.community.service.CommunityService;
 import io.github._3xhaust.root_server.global.common.ApiResponse;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -13,44 +18,55 @@ public class CommunityController {
 
     // 전체 커뮤니티 정보
     @GetMapping
-    public ApiResponse<> getCommunity(){
-
+    public ApiResponse<Page<CommunityListResponse>> getCommunities(
+            @RequestParam(defaultValue="1") int page,
+            @RequestParam(defaultValue="20") int limit
+    ){
+        Page<CommunityListResponse> communities = communityService.getCommunities(page, limit);
+        return ApiResponse.ok(communities);
     }
 
     @PostMapping
-    public ApiResponse<T> createCommunity(
-            @RequestParam() String name
-    ){
+    public ApiResponse<CommunityResponse> createCommunity(
+            Authentication authentication,
 
+            @RequestParam
+            @Valid
+            CreateCommunityRequest request
+    ){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal(); //로그인 유저 정보
+        CommunityResponse newCommunity = communityService.createCommunity(userDetails.getUsername(), request);
+        return ApiResponse.ok(newCommunity, "new roots are made!");
     }
 
     // 특정 커뮤니티 정보
     @GetMapping("/{communityId}")
-    public ApiResponse getCommunityById(
+    public ApiResponse<CommunityResponse> getCommunityById(
             @PathVariable Long communityId
     ){
-
+        CommunityResponse community = communityService.getCommunityById(communityId);
+        return ApiResponse.ok(community);
     }
 
     @PostMapping("/{communityId}/posts")
-    public ApiResponse createCommunityPost(){
+    public ApiResponse<> createCommunityPost(){
 
     }
 
     @GetMapping("/{communityId}/posts")
-    public ApiResponse getCommunityPost(){
+    public ApiResponse<> getCommunityPost(){
 
     }
 
     @GetMapping("/{communityId}/posts/{postId}")
-    public ApiResponse getCommunityPostById(
+    public ApiResponse<> getCommunityPostById(
             @PathVariable Long postId
     ){
 
     }
 
     @PutMapping("/{communityId}/posts/{postId}")
-    public ApiResponse updateCommunityPost(
+    public ApiResponse<> updateCommunityPost(
             Authentication authentication,
             @PathVariable Long postId,
             @RequestBody @Vaild UpdateCommunityPostRequest request
@@ -60,7 +76,7 @@ public class CommunityController {
     }
 
     @DeleteMapping("/{communityId}/posts/{postId}")
-    public ApiResponse DeleteCommunityPost(
+    public ApiResponse<> DeleteCommunityPost(
             Authentication authentication,
             @PathVariable Long postId
     ){
